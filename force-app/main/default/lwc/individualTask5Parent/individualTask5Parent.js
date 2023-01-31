d15f7d9e8a9beeabfb4782e9b58291193cba3210
Algirdas Stricka <algirdasstricka@yahoo.com>
2023-01-31 20:44:15 +0200

    COmmit

--- force-app/main/default/lwc/individualTask5Parent/individualTask5Parent.js
+++ force-app/main/default/lwc/individualTask5Parent/individualTask5Parent.js
@@ -7,11 +7,15 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class IndividualTask5Parent extends LightningElement {
 
+    eventName = '';
+
+    hasEventName = false;
     handleMyEvent(event) {
-        const message = event.detail;
+        this.eventName = event.detail;
+        this.hasEventName = true;
         const toastEvent = new ShowToastEvent({
             title: 'Message got',
-            message: 'Hello, ' + message,
+            message: 'Hello, ' + this.eventName,
             variant: 'info'
         });
         this.dispatchEvent(toastEvent);
